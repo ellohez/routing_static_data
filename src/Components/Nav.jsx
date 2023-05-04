5e5475edf45cde3f7bd8988a46e31e932acdb284
Helen Pickavance <helenpjobs@gmail.com>
2023-05-04 10:43:24 +0100

    Init commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,28 @@
+import { NavLink, NavItem } from 'reactstrap';
+
+const Nav = () => {
+    return (
+        <div>
+            <nav></nav>
+        </div>
+        // <Nav tabs>
+        //     <NavItem>
+        //         <NavLink active href="/">
+        //             Home
+        //         </NavLink>
+        //     </NavItem>
+        //     <NavItem>
+        //         <NavLink href="#">
+        //             Another Link
+        //         </NavLink>
+        //     </NavItem>
+        //     <NavItem>
+        //         <NavLink disabled href="#">
+        //             Disabled Link
+        //         </NavLink>
+        //     </NavItem>
+        // </Nav>
+    );
+};
+
+export default Nav;
---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <>
+            <h2>Oh No! Nothing to see here.</h2>
+            <img src="https://http.cat/404" alt="404 cat with head in the paper"></img>
+        </>
+    );
+};
+
+export default NotFound;
